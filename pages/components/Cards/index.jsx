c9110ce44e1c52bf592907d7e7597d17ec95560c
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-03-07 04:47:47 +0600

    first commit

---
+++ pages/components/Cards/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import s from "../syle.module.scss";
+import Image from "next/image";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+const Cads = ({ data }) => {
+  const toast = useToast()
+
+  return (
+    <>
+      <div className={s.block}>
+        <div className={s.img}>
+          <div className={s.image}>
+            <p>{data.data}</p>
+            <Image className={s.image} alt="/" src={data.img} width={200} height={1} />
+          </div>
+
+          <div className={s.info}>
+            <div className={s.info_block}>
+              <h4 className={s.h4}>{data.name}</h4>
+              <p>{data.type}</p>
+              <p>
+                {data.city},{data.data}
+              </p>
+              <p>{data.Main}</p>
+            </div>
+            <div className={s.price}>
+              <div className={s.price_ps} >
+                <p className={s.price_p}>Цена</p>
+                <p  className={s.price_p}>{data.price} c</p>
+              </div>
+              <Button isDisabled
+                className={s.btn}
+                onClick={() =>
+                  toast({
+                    title: 'Account created.',
+                    description: "We've created your account for you.",
+                    status: 'success',
+                    duration: 9000,
+                    isClosable: true,
+                  })
+                }
+              >
+                Купить
+              </Button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr className={s.hr} />
+    </>
+  );
+};
+
+export default Cads;
